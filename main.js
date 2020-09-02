ac366105da3b9525f7180ce9b2af125dacb0e8b3
Deanna Lampley <70338342+deannalampley@users.noreply.github.com>
2020-09-02 13:48:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -36,27 +36,6 @@ $(function(){
   });  
       
 
-// 			function myFunction() {
-// 	          		let copytext = $("#copytext")
-// 				let holdtext = $("#holdtext")
-// 				holdtext.innerText = hold.innerText;
-// 				Copied = holdtext.createTextRange();
-// 				Copied.execCommand("Copy");
-// 			}
-
-					const span = $("#copytext");
-					const copy_btn = $(".copy_btn")
-					span.onclick = function() {
-					  document.execCommand("copy");
-					}
-
-					span.addEventListener("copy", function(event) {
-					  event.preventDefault();
-					  if (event.clipboardData) {
-					    event.clipboardData.setData("text/plain", copy_btn.textContent);
-					    console.log(event.clipboardData.getData("text"))
-					  }
-					});
 	
 	
 });
